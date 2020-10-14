5d118278cf15d373e2f38b9652ef76f78b8ce108
mohsen <mohsenabedini79OO@gmail.com>
2020-10-14 09:44:52 -0700

    basic element is added

---
+++ app/src/main/java/com/example/marketapp/Model/ProductModel.java
@@ -0,0 +1,58 @@
+package com.example.marketapp.Model;
+
+public class ProductModel {
+    private String id;
+    private String title;
+    private String description;
+    private String currentPrice;
+    private String prePrice;
+    private String imageUrl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(String currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public String getPrePrice() {
+        return prePrice;
+    }
+
+    public void setPrePrice(String prePrice) {
+        this.prePrice = prePrice;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ app/src/main/java/com/example/marketapp/View/CustomView.java
@@ -0,0 +1,42 @@
+package com.example.marketapp.View;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import static com.example.marketapp.R.layout.custom_view;
+
+//this class is a custom view for main activity
+
+public class CustomView extends LinearLayout {
+    public CustomView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+    public void init(){
+        View view= LayoutInflater.from(getContext()).inflate(custom_view,this,true);
+
+    }
+}
